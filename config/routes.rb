e9b2147e24af39d12a8600197b586df038b62b9f
AJ Jalusi <ajalusi26@gmail.com>
2022-03-06 15:39:24 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   resources :liked_comments
   resources :follows
   resources :comments
+
+    #log in/out
+    post '/login', to: 'session#login'
+    get '/logout', to: 'session#logout'
+    get 'is_logged_in', to: 'session#is_logged_in'
+
+    
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
